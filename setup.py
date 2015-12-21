e8be5f5ae56cbbd8a82876b52551a5d933bdd96c
James Nowell <jnowell@savi.com>
2015-12-21 11:37:13 -0500

    PYPI related changes

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name='ticks',
     version='0.1.0',
+    author='James Nowell',
+    author_email='jnowell129@gmail.com',
+    url='https://github.com/jamescnowell/ticks',
+    license='WTFPL',
     description='Command line stock ticker',
+    long_description='Command line stock ticker',    
     packages=find_packages(),
     install_requires=['requests', 'tabulate'],
     entry_points={
