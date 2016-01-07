825eb384592d67bb0771a88a95d187fd1fac0c0f
James Nowell <jnowell@savi.com>
2016-01-07 13:56:02 -0500

    Bump bug fix for decimal places

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ticks',
-    version='0.1.0',
+    version='0.1.1',
     author='James Nowell',
     author_email='jnowell129@gmail.com',
     url='https://github.com/jamescnowell/ticks',
