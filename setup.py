5c2ea3a5334294b1f12f85e4d4ae02ce8ef608c9
James Nowell <jnowell129@gmail.com>
2016-01-07 21:38:36 -0500

    Changed to module instead of package in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='ticks',
-    version='0.1.2',
+    version='0.1.3',
     author='James Nowell',
     author_email='jnowell129@gmail.com',
     url='https://github.com/jamescnowell/ticks',
     license='WTFPL',
     description='Command line stock ticker',
     long_description='Command line stock ticker',    
-    packages=find_packages(),
+    py_modules=['ticks'],
     install_requires=['requests', 'tabulate'],
     entry_points={
         'console_scripts': [
